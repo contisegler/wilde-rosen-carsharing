67734f637c2e312f785df67f860ca371ea0aeada
Hendrik <contisegler@users.noreply.github.com>
2025-05-30 00:50:58 +0200

    add bootstrap

--- app.vue
+++ app.vue
@@ -1,6 +1,9 @@
 <template>
   <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
+    <h1> Using boostrap 5 with Nuxt </h1>
+    <button class="btn btn-primary"> share </button>
+    <div class="alert alert-dark" role="alert">
+      A simple dark alert—check it out!
+    </div>
   </div>
 </template>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,16 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2024-11-01',
-  devtools: { enabled: true }
+  compatibilityDate: '2025-05-01',
+  devtools: { enabled: true },
+  css: ['~/assets/main.scss'],
+  vite: {
+    css: {
+      preprocessorOptions: {
+        scss: {
+          // This suppresses deprecation warnings from dependencies
+          quietDeps: true
+        }
+      }
+    }
+  }
 })
