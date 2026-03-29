ff4c545ef10609dae20e60cb6aa892d9a32342d2
Hendrik <contisegler@users.noreply.github.com>
2026-03-30 00:25:38 +0200

    fix incorrect page title in Jogger vehicle page

--- pages/jogger.vue
+++ pages/jogger.vue
@@ -4,7 +4,7 @@
 
 <template>
   <DefaultPageStructure>
-    <template #title>Kona</template>
+    <template #title>Jogger</template>
     <CarViewer :car-id="'jogger'" />
   </DefaultPageStructure>
 </template>
